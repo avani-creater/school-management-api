860674af840a0094a8d3ea033a4e49f886b32fd0
avani-creator <avisharma1091998@gmail.com>
2025-05-13 04:19:13 +0530

    Updated DB configuration and fixed errors

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 import mysql from 'mysql2';
 import dotenv from 'dotenv';
+import fs from 'fs';
 
 dotenv.config();
 
@@ -8,7 +9,10 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+  ssl: {
+    ca: fs.readFileSync('./certs/ca.cert')
+  }
 });
 
 export default pool.promise();
